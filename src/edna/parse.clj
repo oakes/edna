e2f835f5260d9f527bb73f1536af9ae820335ebb
oakes <zach@sekao.net>
2018-03-20 19:46:25 -0400

    Move parsing to edna.parse

---
+++ src/edna/parse.clj
@@ -0,0 +1,59 @@
+(ns edna.parse
+  (:require [clojure.spec.alpha :as s]
+            [alda.lisp.instruments.midi]
+            [alda.lisp.model.instrument :refer [*stock-instruments*]]))
+
+(def instruments (->> *stock-instruments* keys (map keyword) set))
+
+(def octave-operators #{\+ \-})
+
+(def digits #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9})
+
+(def notes #{\c \d \e \f \g \a \b})
+
+(def pitches #{\# \= \_})
+
+(s/def ::note-parts (s/cat
+                      :octave-operator (s/? octave-operators)
+                      :octaves (s/* digits)
+                      :note notes
+                      :pitch (s/* pitches)))
+
+(defn parse-note [note]
+  (if-not (keyword? note)
+    :clojure.spec.alpha/invalid
+    (s/conform ::note-parts (seq (name note)))))
+
+(defn note? [x]
+  (not= :clojure.spec.alpha/invalid (parse-note x)))
+
+(s/def ::note note?)
+(s/def ::rest #{:r})
+
+(s/def ::octave number?)
+(s/def ::length number?)
+(s/def ::attrs (s/keys :opt-un [::octave ::length ::note]))
+(s/def ::note-attrs (s/keys :opt-un [::octave ::length] :req-un [::note]))
+
+(s/def ::chord (s/coll-of
+                 (s/or
+                   :note ::note
+                   :rest ::rest
+                   :attrs ::note-attrs)
+                 :kind set?))
+
+(s/def ::score (s/cat
+                   :instrument (s/? instruments)
+                   :parts (s/* ::part)))
+
+(s/def ::part (s/alt
+                :note ::note
+                :rest ::rest
+                :octave integer?
+                :attrs ::attrs
+                :chord ::chord
+                :concurrent-score (s/coll-of ::score :kind set?)
+                :score (s/spec ::score)))
+
+(def parse (partial s/conform ::score))
+
