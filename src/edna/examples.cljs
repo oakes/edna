31b2180aa80ce43cbf44f5b30ce34b06fec73a37
oakes <zach@sekao.net>
2018-04-08 17:15:40 -0400

    Throw error if attempt is made to edit

--- src/edna/examples.cljs
+++ src/edna/examples.cljs
@@ -3,14 +3,18 @@
   (:require-macros [edna.examples :refer [edna->data-uri]]
                    [dynadoc.example :refer [defexample]]))
 
+(defn ^:private init-card [card uri]
+  (when (-> card .-childNodes .-length (not= 0))
+    (throw (js/Error. "These examples can't be edited, because edna needs the JVM to generate music.")))
+  (set! (.-textAlign (.-style card)) "center")
+  (reagent.core/render-component [:audio {:src uri :controls true}] card))
+
 (def example-1 (edna->data-uri [:piano :c]))
 
 (defexample example-1
   {:doc "Hit middle c on a piano"
    :with-focus [focus [:piano :c]]
    :with-card card}
-  (set! (.-textAlign (.-style card)) "center")
-  (reagent.core/unmount-component-at-node card)
-  (reagent.core/render-component [:audio {:src example-1 :controls true}] card)
+  (init-card card example-1)
   nil)
 
