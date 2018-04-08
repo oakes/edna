32d075d94605ee13abbaa9ae9bdf6088879f5219
oakes <zach@sekao.net>
2018-04-08 19:06:23 -0400

    Add more examples

--- src/edna/examples.cljs
+++ src/edna/examples.cljs
@@ -13,21 +13,81 @@
     (set! (.-verticalAlign (.-style elem)) "middle")
     (.appendChild card elem)))
 
-(def example-1-single-notes (edna->data-uri [:piano :c :d :e :f]))
+(def aeriths-theme (edna->data-uri
+                     [:piano {:octave 4
+                              :tempo 74}
+                      [1/8 #{:-d :-a :e :f#} :a 1/2 #{:f# :+d}]
+                      [1/8 #{:-e :e :+c} :a 1/2 #{:c :e}]
+                      [1/8 #{:-d :-a :e :f#} :a :+d :+c# :+e :+d :b :+c#]
+                      [1/2 #{:-e :c :a} 1/2 #{:c :e}]]))
 
-(defexample example-1-single-notes
+(defexample aeriths-theme
+  {:doc "A tune from Final Fantasy VII"
+   :with-focus [focus [:piano {:octave 4
+                               :tempo 74}
+                       [1/8 #{:-d :-a :e :f#} :a 1/2 #{:f# :+d}]
+                       [1/8 #{:-e :e :+c} :a 1/2 #{:c :e}]
+                       [1/8 #{:-d :-a :e :f#} :a :+d :+c# :+e :+d :b :+c#]
+                       [1/2 #{:-e :c :a} 1/2 #{:c :e}]]]
+   :with-card card}
+  (init-card card aeriths-theme)
+  nil)
+
+(def dueling-banjos (edna->data-uri
+                      #{[:banjo {:octave 3} 1/16 :b :+c 1/8 :+d :b :+c :a :b :g :a]
+                        [:guitar {:octave 3} 1/16 :r :r 1/8 :g :r :d :r :g :g :d]}))
+
+(defexample dueling-banjos
+  {:doc "Put different pieces in a set to make them play simultaneously"
+   :with-focus [focus #{[:banjo {:octave 3} 1/16 :b :+c 1/8 :+d :b :+c :a :b :g :a]
+                        [:guitar {:octave 3} 1/16 :r :r 1/8 :g :r :d :r :g :g :d]}]
+   :with-card card}
+  (init-card card dueling-banjos)
+  nil)
+
+(def intro-1-single-notes (edna->data-uri [:piano :c :d :e :f]))
+
+(defexample intro-1-single-notes
   {:doc "A few notes, one at a time"
    :with-focus [focus [:piano :c :d :e :f]]
    :with-card card}
-  (init-card card example-1-single-notes)
+  (init-card card intro-1-single-notes)
   nil)
 
-(def example-2-chord (edna->data-uri [:piano #{:c :d}]))
+(def intro-2-octave (edna->data-uri [:piano :c :d {:octave 3} :c :d]))
 
-(defexample example-2-chord
-  {:doc "A chord"
-   :with-focus [focus [:piano  #{:c :d}]]
+(defexample intro-2-octave
+  {:doc "By default you're on the 4th octave, but you can change it"
+   :with-focus [focus [:piano :c :d {:octave 3} :c :d]]
    :with-card card}
-  (init-card card example-2-chord)
+  (init-card card intro-2-octave)
+  nil)
+
+(def intro-3-octave (edna->data-uri [:piano :c :-c :+2c]))
+
+(defexample intro-3-octave
+  {:doc "You can change an individual note's relative octave with a + or - inside the keyword.
+   If you want to change by more than one octave, just put a number after."
+   :with-focus [focus [:piano :c :-c :+2c]]
+   :with-card card}
+  (init-card card intro-3-octave)
+  nil)
+
+(def intro-4-length (edna->data-uri [:piano :c :d 1/2 :e :f]))
+
+(defexample intro-4-length
+  {:doc "By default, notes are 1/4 length, but you can change that as well"
+   :with-focus [focus [:piano :c :d 1/2 :e :f]]
+   :with-card card}
+  (init-card card intro-4-length)
+  nil)
+
+(def intro-5-chord (edna->data-uri [:piano #{:c :d}]))
+
+(defexample intro-5-chord
+  {:doc "A chord is just notes in a set"
+   :with-focus [focus [:piano #{:c :d}]]
+   :with-card card}
+  (init-card card intro-5-chord)
   nil)
 
