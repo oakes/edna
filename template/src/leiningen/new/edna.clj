479a4d15523bb2402bc553764f9ea603a4481f66
oakes <zach@sekao.net>
2018-07-05 09:45:02 -0400

    Fix indentation

--- template/src/leiningen/new/edna.clj
+++ template/src/leiningen/new/edna.clj
@@ -14,13 +14,13 @@
 
 (def initial-score
   "[:piano {:octave 4
-           :tempo 74}
-   
-   1/8 #{:-d :-a :e :f#} :a 1/2 #{:f# :+d}
-   1/8 #{:-e :e :+c} :a 1/2 #{:c :e}
-   
-   1/8 #{:-d :-a :e :f#} :a :+d :+c# :+e :+d :b :+c#
-   1/2 #{:-e :c :a} 1/2 #{:c :e}]")
+         :tempo 74}
+ 
+ 1/8 #{:-d :-a :e :f#} :a 1/2 #{:f# :+d}
+ 1/8 #{:-e :e :+c} :a 1/2 #{:c :e}
+ 
+ 1/8 #{:-d :-a :e :f#} :a :+d :+c# :+e :+d :b :+c#
+ 1/2 #{:-e :c :a} 1/2 #{:c :e}]")
 
 (defn edna-data [name]
   (let [sanitized-name (sanitize-name name)]
